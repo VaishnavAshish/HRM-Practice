e8880eefa306ca9ef51e4f8a0237e4ccb4f20918
ruchika <ruchika.mittal@athenalogics.com>
2019-04-15 17:52:47 +0530

    change in quickbook

--- controllers/quickbook.js
+++ controllers/quickbook.js
@@ -94,7 +94,7 @@ exports.disconnectQuickbook = (req,res) =>{
             console.log('companySetting');
             console.log(companySetting.rows[0]);
             if(companySetting.rows[0].quickbook_token!=null){
-              let quickbook_token = JSON.parse(companySetting.rows[0].quickbook_token.token);
+              let quickbook_token = JSON.parse(companySetting.rows[0].quickbook_token).token;
               console.log('quickbook_token');
               // console.log(quickbook_token);
               let tokenJSON = {
@@ -105,7 +105,7 @@ exports.disconnectQuickbook = (req,res) =>{
                 "access_token":quickbook_token.access_token
               }
               console.log(tokenJSON);
-              companySetting.rows[0].quickbook_token.revoke(tokenJSON)
+              JSON.parse(companySetting.rows[0].quickbook_token).revoke(tokenJSON)
               .then(function(authResponse) {
                 console.log('Tokens revoked : ' + JSON.stringify(authResponse.json()));
                 client.query('UPDATE SETTING set quickbook_token=$1 where company_id=$2 RETURNING id',[null, req.user.company_id], function(err, updatedSetting) {
