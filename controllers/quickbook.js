cde90cabfff9ce7a4f5421fb5d18d3af982c05e4
ruchika <ruchika.mittal@athenalogics.com>
2019-04-24 17:00:19 +0530

    change in error message

--- controllers/quickbook.js
+++ controllers/quickbook.js
@@ -37,7 +37,7 @@ exports.getAuthCode = (req,res) => {
              console.log(oauth2_token_json);
              pool.connect((err, client, done) => {
                let webhook_url = process.env.BASE_URL+'/quickbookInvoiceUpdate/'+req.user.company_id;
-               
+
                  client.query('UPDATE SETTING set quickbook_token=$1,quickbook_webhook_url=$2 where company_id=$3 RETURNING id',[oauthClient,webhook_url ,req.user.company_id], function(err, updatedSetting) {
                    if (err){
                      handleResponse.shouldAbort(err, client, done);
@@ -328,8 +328,8 @@ exports.postInvoiceToQuickbook = (req,res) => {
                      })
                  })
                  .catch(function(e) {
-                     console.error("The error message for refreshing token  is :"+e.originalMessage);
-                     console.error(e.intuit_tid);
+                     console.error("The error message for refreshing token  is :");
+                     console.error(e);
                      handleResponse.handleError(res, e, ' Error in refreshing token'+e);
                  });
 
