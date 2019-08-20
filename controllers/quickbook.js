e92e602286feab9dfacd2d068771c3eed2e5e3af
Ruchika <ruchika.mittal@athenalogics.com>
2019-08-20 17:46:48 +0530

    change in quickbook instance callback

--- controllers/quickbook.js
+++ controllers/quickbook.js
@@ -163,13 +163,13 @@ exports.getAuthCode = (req,res) => {
                       console.log(oauthClient.token["realmId"]);
                       if(previousQuickbookCompanyId == oauthClient.token["realmId"]){
                         console.log('------------oauthClient--------')
-                        let oauthClientObj = {};
+                        /*let oauthClientObj = {};
                         oauthClientObj.environment = oauthClient.environment;
                         oauthClientObj.clientId = oauthClient.clientId,
                         oauthClientObj.clientSecret = oauthClient.clientSecret,
                         oauthClientObj.redirectUri = oauthClient.redirectUri,
                         oauthClientObj.logging = oauthClient.logging
-                        oauthClientObj.token = oauthClient.token
+                        oauthClientObj.token = oauthClient.token*/
 
                         console.log(oauthClientObj);
                         client.query('UPDATE SETTING set quickbook_token=$1,quickbook_enabled=$2 where company_id=$3 RETURNING id',[oauthClientObj ,true,req.user.company_id], function(err, updatedSetting) {
