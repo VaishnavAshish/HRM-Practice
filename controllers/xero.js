da25d45ed9f68bcf26250ac66af432faa12384c9
Ruchika <ruchika.mittal@athenalogics.com>
2019-08-21 12:10:16 +0530

    change in comment user image

--- controllers/xero.js
+++ controllers/xero.js
@@ -43,11 +43,11 @@ exports.getAuthCodeXero = (req,res) => {
   console.log('req');
   console.log('------------session requestToken------------')
   console.log(req.session.passport.user.xeroRequestToken);
-  console.log('------------requestToken------------')
-  console.log(requestToken)
 
   (async () => {
     try {
+      console.log('------------requestToken------------')
+      console.log(requestToken)
       const oauth_verifier = req.query.oauth_verifier;
       if(requestToken == null){
         console.log('inside request token is null')
