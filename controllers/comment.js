7067b5e255ae68a309a1ce0fd7d10b9c09bc6a1b
Ruchika <ruchika.mittal@athenalogics.com>
2019-09-19 18:21:32 +0530

    add email button on comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -4,6 +4,7 @@ const handleResponse = require('./page-error-handle');
 const moment = require('moment-timezone');
 const setting = require('./company-setting');
 const commonController = require('./common-functions');
+const email = require('./email');
 let companyDefaultTimezone;
 
 function dateFormat(gDate) {
@@ -140,6 +141,8 @@ exports.deleteConversation = (req, res) => {
 }
 
 exports.postAddComment = (req, res) => {
+  // console.log('---req.body postAddComment---')
+  // console.log(req.body)
   req.assert('newCommentTitle', 'Comment Title cannot be blank').notEmpty();
 
   const errors = req.validationErrors();
@@ -168,7 +171,7 @@ exports.postAddComment = (req, res) => {
                     handleResponse.shouldAbort(err, client, done);
                     handleResponse.handleError(res, err, ' Error in adding project comment to the database');
                   } else {
-                    client.query('UPDATE PROJECT_COMMENT SET modified_date = $1 WHERE id = $2', ['now()',req.body.conversation_id], function (err, updatedProjectConversation) {
+                    client.query('UPDATE PROJECT_COMMENT SET modified_date = $1 WHERE id = $2 RETURNING *', ['now()',req.body.conversation_id], function (err, updatedProjectConversation) {
                       if (err) {
                         handleResponse.shouldAbort(err, client, done);
                         handleResponse.handleError(res, err, ' Error in adding project conversation to the database');
@@ -179,8 +182,61 @@ exports.postAddComment = (req, res) => {
                             handleResponse.shouldAbort(err, client, done);
                             handleResponse.handleError(res, err, ' Error in committing transaction');
                           } else {
-                            done();
-                            handleResponse.sendSuccess(res,'Project Comment added successfully',{});
+                            if(req.body.sendemail == true){
+                              // client.query('SELECT user_id FROM PROJECT_ASSIGNMENT where project_id =$1', [req.body.project_id],function (err, projectResourceIdList) {
+                              //   if (err) {
+                              //     handleResponse.shouldAbort(err, client, done);
+                              //     handleResponse.handleError(res, err, ' Error in finding project assignment users in the database');
+                              //   } else {
+                                  client.query('SELECT name FROM PROJECT where id =$1', [req.body.project_id],function (err, projectData) {
+                                    if (err) {
+                                      handleResponse.shouldAbort(err, client, done);
+                                      handleResponse.handleError(res, err, ' Error in finding project data in the database');
+                                    } else {
+                                      //let userIdList = projectResourceIdList.rows.concat(updatedProjectConversation.rows[0].resource_id);
+                                      client.query('SELECT id,email ,first_name ,last_name from users where id In (SELECT user_id FROM PROJECT_ASSIGNMENT where project_id =$1)', [req.body.project_id],function (err, selectedProjectUserList) {
+                                        if (err) {
+                                          handleResponse.shouldAbort(err, client, done);
+                                          handleResponse.handleError(res, err, ' Error in finding project user in the database');
+                                        } else {
+                                            let selectedProjectConverationUser = selectedProjectUserList.rows.filter(user => user.id == updatedProjectConversation.rows[0].resource_id)[0];
+                                            updatedProjectConversation.rows[0].email = selectedProjectConverationUser.email
+                                            updatedProjectConversation.rows[0].first_name = selectedProjectConverationUser.first_name
+                                            updatedProjectConversation.rows[0].last_name = selectedProjectConverationUser.last_name
+
+                                            client.query('SELECT id,message,modified_date,parent_id,resource_id,(SELECT email from users where id = resource_id) as email , (SELECT first_name from users where id = resource_id) as first_name , (SELECT last_name from users where id = resource_id) as last_name FROM PROJECT_COMMENT pc where parent_id=$1 AND project_id=$2 ORDER BY modified_date desc LIMIT 10', [req.body.conversation_id,req.body.project_id],function (err, selectedProjectComment) {
+                                              if (err) {
+                                                handleResponse.shouldAbort(err, client, done);
+                                                handleResponse.handleError(res, err, ' Error in adding project comment to the database');
+                                              } else {
+                                                // console.log('updatedProjectConversation.rows[0]');
+                                                // console.log(updatedProjectConversation.rows[0]);
+                                                // console.log(selectedProjectComment.rows);
+                                                updatedProjectConversation.rows[0].modified_date = moment.tz(updatedProjectConversation.rows[0].modified_date, companyDefaultTimezone).format('MM-DD-YYYY hh:mm:ss');
+                                                selectedProjectComment.rows.forEach(data=>{
+                                                    data.modified_date = moment.tz(data.modified_date, companyDefaultTimezone).format('MM-DD-YYYY hh:mm:ss');
+                                                })
+                                                let projectResourceEmailList = ''
+                                                selectedProjectUserList.rows.map(userData => projectResourceEmailList += userData.email+',');
+                                                // console.log(projectResourceEmailList)
+                                                // console.log(projectData.rows)
+
+                                                sendConversationThread(req,res,updatedProjectConversation.rows[0],selectedProjectComment.rows,projectResourceEmailList,projectData.rows[0],function(){
+                                                  done();
+                                                  handleResponse.sendSuccess(res,'Project Comment added successfully',{});
+                                                });
+                                              }
+                                            })
+                                        }
+                                      })
+                                    }
+                                  })
+                              //   }
+                              // })
+                            }else{
+                                done();
+                                handleResponse.sendSuccess(res,'Project Comment added successfully',{});
+                            }
                           }
                         })
                       }
@@ -194,6 +250,124 @@ exports.postAddComment = (req, res) => {
       });
     }
 }
+
+function getRandomColor() {
+     var letters = 'BCDEF'.split('');
+     var color = '#';
+     for (var i = 0; i < 6; i++ ) {
+         color += letters[Math.floor(Math.random() * letters.length)];
+     }
+     return color;
+ }
+
+
+sendConversationThread = (req, res, conversation_thread, commentList,projectResourceEmailList,projectData,next) => {
+  let serverName = process.env.BASE_URL;
+  let commentListHTML = ``;
+  commentList.forEach(commentData => {
+    commentHTML = `<tr>
+                      <td  valign="top" style=" font-size:14px;font-family: arial,sans-serif; padding-top:10px; border-top: 1px solid #eee; color: #999; width:50px;">
+                          <span style="border-radius: 50%; height:48px; width: 48px; text-align:center; line-height: 48px; background-color:${getRandomColor()}; display: inline-block; text-transform: uppercase; letter-spacing: 1px; color: white; font-size: 16px;">
+                              ${commentData.first_name.substring(0,1).toUpperCase()}${commentData.last_name?commentData.last_name.substring(0,1).toUpperCase():''}
+                          </span>
+                      </td>
+                      <td   style=" font-size:14px;font-family: arial,sans-serif; padding:10px 10px 25px 10px; border-top: 1px solid #eee; ">
+                          <div style="font-size:11px; color: #999;">
+                              ${commentData.modified_date} / By: ${commentData.first_name} ${commentData.last_name?commentData.last_name:''}
+                          </div>
+                          <div style="color: #505661; line-height: 1.5; font-size: 13px;">
+                              ${commentData.message}
+                          </div>
+                      </td>
+                  </tr>`
+        commentListHTML += commentHTML;
+  })
+  console.log(commentListHTML);
+  //<a href="javascript:void(0);" target="_blank"><img src="${process.env.BASE_URL}/getCompanyLogoForEmail/${req.user.company_id}" alt="company_logo" style="max-width:150px;"></a>
+
+  let html = `<html><head></head><body>
+                <div style="background-color: #f7f8f9;">
+                  <table border="0" cellpadding="0" cellspacing="0"  width="100%" bgcolor="#f7f8f9">
+                    <tbody>
+                        <tr>
+                            <td  valign="top" align="center" style="padding-top: 20px; padding-bottom: 10px;">
+                                <img src="${process.env.BASE_URL}/img/krow-logo.png" alt=""  width="84" height="29">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td  valign="top" align="center">
+                                <table border="0" cellpadding="0" cellspacing="0" height="100%" width="800px">
+                                    <tr>
+                                        <td  valign="top" >
+                                            <table cellpadding="0" cellspacing="0"  width="100%" style="background: #fff; border: 1px solid #eee; margin: 0; padding: 30px; ">
+                                                <tbody>
+                                                    <tr>
+                                                        <td  valign="top" >
+                                                            <div style="font-family: arial,sans-serif; font-size:16px;  margin-bottom: 15px;">
+                                                                <span style="color: #999;">
+                                                                    Project :
+                                                                </span>
+                                                                ${projectData.name}
+                                                            </div>
+                                                            <div style="border-left: 3px solid #006dcc; padding-left: 10px; margin-top: 15px; margin-bottom: 15px;">
+                                                                <div style="font-family: arial,sans-serif; font-size:24px; line-height: 1.5;  color: #006dcc;">
+                                                                        ${conversation_thread.message}
+                                                                </div>
+                                                                <div style="font-family: arial,sans-serif; font-size:11px; font-weight:normal; color: #999;">
+                                                                    Last Modify: ${conversation_thread.modified_date} / By: ${conversation_thread.first_name} ${conversation_thread.last_name?conversation_thread.last_name:''} ${conversation_thread.email}
+                                                                </div>
+                                                            </div>
+                                                            <table border="0" cellpadding="0" cellspacing="0"  width="100%" style="table-layout: fixed;">
+                                                                <tbody>
+                                                                    ${commentListHTML}
+
+                                                                </tbody>
+                                                            </table>
+
+
+                                                            <p style="font-family: arial,sans-serif; font-size:14px; font-weight:normal; margin-bottom: 5px;">
+                                                                Thanks,
+                                                            </p>
+                                                            <p style="font-family: arial,sans-serif; font-size:14px; font-weight:normal; margin-top: 5px;">
+                                                                The Team at Krow
+                                                            </p>
+                                                        </td>
+                                                    </tr>
+
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                  </table>
+                  </div></body></html>`;
+
+
+
+  const mailOptions = {
+    to: projectResourceEmailList,
+    from: '"'+req.user.company_info.name+'"support@krowsoftware.com',
+    subject: "New Activity for Project"+projectData.name,
+    html: html
+  };
+
+  req.mailOptions = mailOptions;
+  // console.log('transpoter');
+  email.sendMail(req, res, function(error, info) {
+    // console.log('transpoter');
+    if (error) {
+      console.log(error);
+      next(error, null);
+    } else {
+      // console.log('Message sent: ' + info.response);
+      next(null, info);
+      /*res.status(200).json({"success": true,"message":"success" });*/
+    }
+  });
+};
 
 exports.getAllRelatedComment = (req, res) => {
   setting.getCompanySetting(req, res ,(err,result)=>{
