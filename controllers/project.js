29dce5569537efa7db0a5f82a3fbd9fc6f36f41e
Ruchika <ruchika.mittal@athenalogics.com>
2019-09-30 11:36:12 +0530

    changes

--- controllers/project.js
+++ controllers/project.js
@@ -214,7 +214,7 @@ exports.updateTaskSortOrder = (req,res) => {
             let previousString = prevSortOrder.substring(0,prevSortOrder.indexOf(dropTaskId)-1)
             let finalSortString = (previousString!=""&&afterString!=""?(previousString+','+afterString):(previousString+afterString))
             if(targetContainerId == null){
-                newTaskSortOrder = finalSortString+','+dropTaskId;
+                newTaskSortOrder = (finalSortString?finalSortString+',':'')+dropTaskId;
             }else{
                 newTaskSortOrder = finalSortString.substring(0,finalSortString.indexOf(targetContainerId))+dropTaskId+','+finalSortString.substring(finalSortString.indexOf(targetContainerId))
             }
@@ -778,9 +778,9 @@ exports.getProjectDetail = (req, res) => {
                       requiredTasks= project.rows[0].task_sort_order.substring(0,project.rows[0].task_sort_order.split(',',process.env.PAGE_RECORD_NO).join(',').length);
                     }
                     console.log(requiredTasks);
-                    let qry=`SELECT t.id ,t.project_id ,t.name ,t.type ,t.start_date at time zone '${companyDefaultTimezone}' as start_date ,t.end_date at time zone '${companyDefaultTimezone}' as end_date ,t.total_hours ,t.billable ,t.completion_date at time zone '${companyDefaultTimezone}' as completion_date ,t.status ,t.include_weekend ,t.description ,t.percent_completed ,t.estimated_hours ,t.completed ,t.assigned_by_name ,t.assigned_user_id ,t.billable_hours ,t.milestone ,t.parent_id ,t.company_id ,t.priority ,t.created_date ,t.updated_date ,t.archived ,t.project_name ,t.record_id  
-                      FROM TASK t 
-                      where id in (${requiredTasks}) 
+                    let qry=`SELECT t.id ,t.project_id ,t.name ,t.type ,t.start_date at time zone '${companyDefaultTimezone}' as start_date ,t.end_date at time zone '${companyDefaultTimezone}' as end_date ,t.total_hours ,t.billable ,t.completion_date at time zone '${companyDefaultTimezone}' as completion_date ,t.status ,t.include_weekend ,t.description ,t.percent_completed ,t.estimated_hours ,t.completed ,t.assigned_by_name ,t.assigned_user_id ,t.billable_hours ,t.milestone ,t.parent_id ,t.company_id ,t.priority ,t.created_date ,t.updated_date ,t.archived ,t.project_name ,t.record_id
+                      FROM TASK t
+                      where id in (${requiredTasks})
                       ORDER BY position(id::text in '${requiredTasks}')`;
                     if(requiredTasks==null){
                       qry='SELECT t.id ,t.project_id ,t.name ,t.type ,t.start_date at time zone \''+companyDefaultTimezone+'\' as start_date ,t.end_date at time zone \''+companyDefaultTimezone+'\' as end_date ,t.total_hours ,t.billable ,t.completion_date at time zone \''+companyDefaultTimezone+'\' as completion_date ,t.status ,t.include_weekend ,t.description ,t.percent_completed ,t.estimated_hours ,t.completed ,t.assigned_by_name ,t.assigned_user_id ,t.billable_hours ,t.milestone ,t.parent_id ,t.company_id ,t.priority ,t.created_date ,t.updated_date ,t.archived ,t.project_name ,t.record_id FROM TASK t where company_id='+req.user.company_id+' AND project_id='+req.query.projectId+' AND archived=false ORDER BY id,project_id,start_date DESC,name LIMIT '+process.env.PAGE_RECORD_NO+' OFFSET 0';
@@ -866,7 +866,7 @@ exports.getProjectDetail = (req, res) => {
                               }
                               taskSortedArr.push(data);
                             });
-                            
+
                             if(project.rows[0].total_task_count){
                               taskTotalCount=project.rows[0].total_task_count;
                             }
@@ -1284,4 +1284,3 @@ exports.checkAndCreateProjectAssignment = (req, res) => {
     });
   });
 }
-
